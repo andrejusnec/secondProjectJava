d88e581bd5e0710dcdf147314727fd4676e129e9
andrejusnec <73877307+andrejusnec@users.noreply.github.com>
2021-01-12 13:03:25 +0200

    HomeWork sorting

--- src/secondprojectjava/SecondProjectJava.java
+++ src/secondprojectjava/SecondProjectJava.java
@@ -12,8 +12,8 @@ public class SecondProjectJava {
     public static void main(String[] args) {
         int[] masyvas = {15, 42, 15, 98, -2000, -47, -10, 3, -14, 36, 40, 10};
         System.out.println("Duotas masyvas:");
-        for(int i = 0; i < masyvas.length; i++){
-        System.out.print(masyvas[i] + " ");
+        for (int i = 0; i < masyvas.length; i++) {
+            System.out.print(masyvas[i] + " ");
         }
         /**
          * ***************DYDEJIMO TVARKA*****************
@@ -46,6 +46,34 @@ public class SecondProjectJava {
             System.out.print(masyvas[i] + " ");
         }
         System.out.println("\n");
+        /**
+         * ******************************************************************
+         */
+        System.out.println("********************************************");
+        int[] m = {1, 2, 3, 4, 5};
+        int[][] rez = new int[m.length][m.length];
+        int[] tmp = new int[m.length];
+        for (int k = 0; k < m.length; k++) {
+            for (int j = 0; j < m.length; j++) {
+                rez[k][j] = m[j];
+            }
+            //*******************************************
+            tmp[m.length - 1] = m[0];
+            for (int i = 1; i < m.length; i++) {
+                tmp[i-1] = m[i];
+            }
+            for (int i = 0; i < m.length; i++) {
+                m[i] = tmp[i];
+            }
+        }
+        //****************************************
+        for (int a = 0; a < m.length; a++) {
+            for (int b = 0; b < m.length; b++) {
+                System.out.print(rez[a][b] + " ");
+            }
+            System.out.println("");
+        }
+        System.out.println("********************************************");
     }
 
 }
